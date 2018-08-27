bfd9db701ce762be88a9fb964a9ff9c98d5d4bae
Rick Klomp <r.klomp@cs.stir.ac.uk>
2018-08-27 17:34:52 +0100

    Website changes

--- web/analyse.php
+++ web/analyse.php
@@ -4,21 +4,22 @@ $explanation = <<<XYZ
 <br>
 <h2>Explanation of output</h2>
 <p>(Note: this currently only describes the default verbosity output mode)</p>
-<p>For every unique branch of the supplied output script, the tool will print a verdict under a distinct “— Symbolic evaluation report of execution branch <em>i</em>” section</p>
+<p>For every unique branch of the supplied output script, the tool will print inferred information under a distinct "--- Symbolic evaluation report of execution branch <em>i</em>" section</p>
 <p>In this section, the following information is printed:</p>
 <ul>
-<li>The respective branch’s decision points are shown (i.e. for every encountered IF operation, is its True branch traversed or is its False branch traversed?)</li>
+<li>The respective branch's decision points are shown (i.e. for every encountered IF operation, is its True branch traversed or is its False branch traversed?)</li>
 <li>The symbolic stack that the input script must supply</li>
 <li>The inferred constraints (on these supplied variables)</li>
 <li>The final resulting symbolic stack (note, at this point we have already performed an additional OP_VERIFY, thus the constraint that the resulting full execution of this branch must end with a true value on the stack is already in the inferred constraints set)</li>
 </ul>
 XYZ;
 
-$request = "localhost:3000/analyse?output_script="
+$request = "139.153.253.238:3000/analyse?output_script="
          . urlencode($_GET["output_script"])
          . "&name=" . urlencode($_GET["name"])
          . "&email=" . urlencode($_GET["email"])
-         . "&verbosity=" . urlencode($_GET["verbosity"]);
+         . "&verbosity=" . urlencode($_GET["verbosity"])
+         . "&ip=" . urlencode($_SERVER["REMOTE_ADDR"]);
 #echo $request;
 
 $ch = curl_init($request);

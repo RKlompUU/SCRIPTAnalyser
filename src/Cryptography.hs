0c2ec1362ab2cb9b08b6aaa993a35616d0be5468
Rick Klomp <r.klomp@students.uu.nl>
2018-09-13 16:01:03 +0100

    debuggin

--- src/Cryptography.hs
+++ src/Cryptography.hs
@@ -3,8 +3,6 @@ module Cryptography where
 import Crypto.Hash
 
 import qualified Data.ByteString.Lazy as BS
-import qualified Data.ByteArray as BA
-import qualified Data.ByteArray.Encoding as BAE
 
 hash256 :: BS.ByteString -> BS.ByteString
 hash256 = sha256 . sha256
@@ -15,11 +13,11 @@ hash160 = ripemd160 . sha256
 sha256 :: BS.ByteString -> BS.ByteString
 sha256 bs =
   BS.fromStrict
-  $ BAE.convertToBase BAE.Base16
+  $ digestToHexByteString
   $ (hashlazy bs :: Digest SHA256)
 
 ripemd160 :: BS.ByteString -> BS.ByteString
 ripemd160 bs =
   BS.fromStrict
-  $ BAE.convertToBase BAE.Base16
+  $ digestToHexByteString
   $ (hashlazy bs :: Digest RIPEMD160)
